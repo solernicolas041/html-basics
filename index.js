31a12ba9516f6a4abed925909c518ae49a5943a8
solernicolas041 <solernicolas041@gmail.com>
2025-12-30 20:45:09 +1100

    html + css file with flexbox layout and added new sections and styles

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log("Hello From Javascript");
+
+/* Data types :
+Undefined, mull, boolean, sting, symbol, number, object
+*/
+var myName = "John Doe"; // string
+myName = 8
+let ourName = "freeCodeCamp"; // string
+const pi = 3.14; // number
+
